fd3de738e6182abfb169c09f224bb1ff711b0304
Charlie Scheel <109446478+CharlieScheel@users.noreply.github.com>
2022-08-04 22:50:17 -0500

    Add files via upload

--- shift_tb.v
+++ shift_tb.v
@@ -2,33 +2,33 @@
 
 module shift_tb();
 
+/* declare variables */
 reg [7:0] in;
 reg add;
 wire [7:0] out;
 
+/* testing shift.v */
 shift uut(in, add, out);
 
 initial begin
 
+    $monitor("%b -> %b", in, out);
+
     in = 8'd0;
     add = 0;
     #5;
-    $display("%b -> %b", in, out);
 
     in = 8'd2;
     add = 1;
     #5;
-    $display("%b -> %b", in, out);
 
     in = 8'd63;
     add = 0;
     #5;
-    $display("%b -> %b", in, out);
 
     in = 8'd255;
     add = 1;
     #5;
-    $display("%b -> %b", in, out);
 
 end
 

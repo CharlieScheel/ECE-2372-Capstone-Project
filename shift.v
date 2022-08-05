a23a4cf944f820c4d76feab5d525cd64f049d3aa
Charlie Scheel <109446478+CharlieScheel@users.noreply.github.com>
2022-08-04 22:43:02 -0500

    Add files via upload

--- shift.v
+++ shift.v
@@ -4,7 +4,7 @@ input [7:0] in;
 input add;
 output [7:0] out;
 
-assign out[6:0] = in >> 1;
-assign out[7] = add;
+assign out[6:0] = in >> 1;  // shifts to the right one
+assign out[7] = add;        // assigns value that was opened up
 
 endmodule

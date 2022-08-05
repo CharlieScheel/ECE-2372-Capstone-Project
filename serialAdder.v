4fdc33f5c6c1f47b9bc08b79cd6e4d6c6fd5513c
Charlie Scheel <109446478+CharlieScheel@users.noreply.github.com>
2022-08-04 22:41:41 -0500

    Add files via upload

--- serialAdder.v
+++ serialAdder.v
@@ -1,6 +1,12 @@
 `include "bitAdder.v"
 `include "shift.v"
 
+/* input: two 8-bit binary numbers and a carry
+ * output: a and b are shifted to the right 1
+           the bitsum of their lost values is stored in the opened value of a 
+           b's lost value looped around
+           whether there is a carry or not
+ */
 module serialAdder(a, b, sum, carryIn, carryOut, bOut);
 
     input [7:0] a, b; 

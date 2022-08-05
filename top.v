19518466dae58c9c493d73ea9e0bd6aa56cb4a04
Charlie Scheel <109446478+CharlieScheel@users.noreply.github.com>
2022-08-04 22:52:36 -0500

    Add files via upload

--- top.v
+++ top.v
@@ -9,9 +9,11 @@ module top(a, b, zFlag, c, nFlag);
     wire [7:0] A1to2, A2to3, A3to4, A4to5, A5to6, A6to7, A7to8, B1to2, B2to3, B3to4, B4to5, B5to6, B6to7, B7to8, newB;
     wire C1to2, C2to3, C3to4, C4to5, C5to6, C6to7, C7to8;
 
+    /* multiply by -1 */
     complement u0(.in(b), .out(newB));
 
     /* There is definitely a more elegant way to do this, but I was not sure how to do so */
+    /* add the negative value to a */
     serialAdder u1(.a(a), .b(newB), .sum(A1to2), .carryIn(1'b0), .carryOut(C1to2), .bOut(B1to2));
     serialAdder u2(.a(A1to2), .b(B1to2), .sum(A2to3), .carryIn(C1to2), .carryOut(C2to3), .bOut(B2to3)); 
     serialAdder u3(.a(A2to3), .b(B2to3), .sum(A3to4), .carryIn(C2to3), .carryOut(C3to4), .bOut(B3to4));
